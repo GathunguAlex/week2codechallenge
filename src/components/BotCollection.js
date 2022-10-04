3126226abd676b35e85055ae88cd1cd593bb721a
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-10-04 04:48:07 +0300

    imported  component Botcard

---
+++ src/components/BotCollection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import botCard from "./BotCard";
+
+function BotCollection() {
+  // Your code here
+  return (
+    <div className="ui four column grid">
+      <div className="row">
+        {/*...and here..*/}
+        Collection of all bots
+      </div>
+    </div>
+  );
+}
+
+export default BotCollection;
